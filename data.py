3c4217ffe7483ce1730b76db9c896db5efb9a8a1
Ofir Lazarov <ofirlazarov@Ofirs-MacBook-Air.local>
2026-05-28 17:49:44 +0300

    Update legal pages, maps data, and refactor legal footer styling

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 # Mock data for Ofoodiez homepage
 
 data = {
-    "slogan": "Great Deals & Happy Hours, exploring restaurants and the good life around the world",
+    "slogan": "Everything new in Tel Aviv before everyone else, Happy Hours, and curated Foodie Maps for 8+ countries around the world.",
     "community": {
         "title": "Community Insider",
         "description": "Join our WhatsApp group to get the latest updates, secret deals, and new resturants and Pop Ups in TLV!",
@@ -9,7 +9,7 @@ data = {
         "link_text": "Join WhatsApp Group"
     },
     "world_maps": [
-         {
+        {
             "country": "Thailand",
             "flag": "🇹🇭",
             "cities": "Bangkok, Phuket, Koh Phi Phi, Krabi",
@@ -20,6 +20,12 @@ data = {
             "flag": "🇬🇷",
             "cities": "Athens, Crete(Chania), Milos, Corfu, Peloponnesus",
             "map_link": "https://maps.app.goo.gl/jsHWQBnM4oQTWGco6" # External link mock
+        },
+        {
+            "country": "Japan",
+            "flag": "🇯🇵",
+            "cities": "Tokyo, Kyoto, Osaka, Okinawa",
+            "map_link": "https://www.google.com/maps/@/data=!3m1!4b1!4m2!11m1!2sNcLlKeOBS1WnlvweSIahcQ?g_ep=CAISEjI2LjIwLjQuOTEzODcwMjQ0MBgAILffASpJLDk0Mjk3Njk5LDk0MjMxMTg8LDk0MjgwNTY4LDQ3MDcxNzA0LDk0MjE4NjQxLDk0MjgyMTM0LDEwMDc5OTg3Nyw5NDI4Njg2OUICSUw%3D"
         },
         {
             "country": "Italy",
@@ -34,28 +40,28 @@ data = {
             "map_link": "https://maps.app.goo.gl/KWjvp1LHRG4dS5Ko6" # External link mock
         },
         {
-            "country": "Spain",
-            "flag": "🇪🇸",
-            "cities": "Coming Soon",
-            "map_link": "#" # Coming soon
-        },
-        {
-            "country": "Japan",
-            "flag": "🇯🇵",
-            "cities": "Tokyo, Kyoto, Osaka, Okinawa",
-            "map_link": "https://www.google.com/maps/@/data=!3m1!4b1!4m2!11m1!2sNcLlKeOBS1WnlvweSIahcQ?g_ep=CAISEjI2LjIwLjQuOTEzODcwMjQ0MBgAILffASpJLDk0Mjk3Njk5LDk0MjMxMTg8LDk0MjgwNTY4LDQ3MDcxNzA0LDk0MjE4NjQxLDk0MjgyMTM0LDEwMDc5OTg3Nyw5NDI4Njg2OUICSUw%3D"
-        },
-        {
             "country": "France",
             "flag": "🇫🇷",
             "cities": "Paris",
             "map_link": "https://www.google.com/maps/@/data=!3m1!4b1!4m2!11m1!2sIRCaLjsaTrOcDzgeWjImsw?g_ep=CAISEjI2LjIwLjQuOTEzODcwMjQ0MBgAILffASpJLDk0Mjk3Njk5LDk0MjMxMTg8LDk0MjgwNTY4LDQ3MDcxNzA0LDk0MjE4NjQxLDk0MjgyMTM0LDEwMDc5OTg3Nyw5NDI4Njg2OUICSUw%3D"
         },
+        {
+            "country": "Philippines",
+            "flag": "🇵🇭",
+            "cities": "Palawan, Boracay, Siargao",
+            "map_link": "https://maps.app.goo.gl/2biLcv2T7bhDSt9s8?g_st=i"
+        },
+        {
+            "country": "Spain",
+            "flag": "🇪🇸",
+            "cities": "Coming Soon",
+            "map_link": "#" # Coming soon
+        },
     ],
     "about": {
         "title": "About Me",
         "text": "Hi, I'm Ofir! I love exploring food scenes around the world and finding the best deals. Ofoodiez is my way of sharing these gems with you.",
-        "contact_link": "mailto:ofir.lazarov@gmail.com",
+        "contact_link": "mailto:contact@ofoodiez.com",
         "contact_text": "Contact Me"
     },
     "extra_page": {
